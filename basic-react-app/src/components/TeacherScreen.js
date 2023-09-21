f6c70149b0ef4ae233cd7ba836e0d2c4991c9a9e
KJY-kpu <rntlls1366@gmail.com>
2023-09-21 14:25:18 +0900

    🔥	: 불필요한 mediasoup 관련 코드 삭제

--- basic-react-app/src/components/TeacherScreen.js
+++ basic-react-app/src/components/TeacherScreen.js
@@ -6,70 +6,6 @@ import os from 'os-browserify';
 const HLS_SERVER_URL = process.env.REACT_APP_HLS_SERVER_URL;
 const SFU_SERVER_URL = process.env.REACT_APP_SFU_SERVER_URL
 
-const mediasoupConfig = Object.freeze({
-  numWorkers: Object.keys(os.cpus()).length,
-  worker: {
-    logLevel: 'debug',
-    logTags: [
-      'rtp',
-      'srtp',
-      'rtcp',
-    ],
-    rtcMinPort: 40000,
-    rtcMaxPort: 49999
-  },
-  router: {
-    mediaCodecs: [
-      {
-        kind: 'audio',
-        mimeType: 'audio/opus',
-        clockRate: 48000,
-        channels: 2
-      },
-      {
-        kind: 'video',
-        mimeType: 'video/VP8',
-        clockRate: 90000,
-        parameters: {
-          'x-google-start-bitrate': 1000
-        }
-      },
-      {
-        kind: 'video',
-        mimeType: 'video/VP9',
-        clockRate: 90000,
-        parameters: {
-          'profile-id': 2,
-          'x-google-start-bitrate': 1000
-        }
-      },
-      {
-        kind: 'video',
-        mimeType: 'video/H264',
-        clockRate: 90000,
-        parameters: {
-          'packetization-mode': 1,
-          'profile-level-id': '42e01f',
-          'level-asymmetry-allowed': 1,
-          'x-google-start-bitrate': 1000
-        }
-      },
-    ]
-  },
-  webRtcTransport: {
-    listenIps: [{ ip: '127.0.0.1', announcedIp: '127.0.0.1' }], // TODO: Change announcedIp to your external IP or domain name
-    enableUdp: true,
-    enableTcp: true,
-    preferUdp: true,
-    maxIncomingBitrate: 1500000
-  },
-  plainRtpTransport: {
-    listenIp: { ip: '127.0.0.1', announcedIp: '127.0.0.1' }, // TODO: Change announcedIp to your external IP or domain name
-    rtcpMux: true,
-    comedia: false
-  }
-});
-
 class SocketQueue {
   constructor() {
     this.queue = new Map();
@@ -253,29 +189,6 @@ function TeacherScreen() {
   const handleSocketClose = () => {
     console.log('handleSocketClose()');
   };
-
-  const getVideoCodecs = () => {
-    const params = new URLSearchParams(window.location.search.slice(1));
-    const videoCodec = params.get('videocodec')
-    console.warn('videoCodec');
-
-    const codec = mediasoupConfig.router.mediaCodecs.find(c => {
-      if (!videoCodec)
-        return undefined;
-
-      return ~c.mimeType.toLowerCase().indexOf(videoCodec.toLowerCase())
-    });
-
-    console.warn('codec', codec);
-    return codec ? codec : {
-      kind: 'video',
-      mimeType: 'video/VP8',
-      clockRate: 90000,
-      parameters: {
-        'x-google-start-bitrate': 1000
-      }
-    };
-  }
 
   const handleSocketError = error => {
     console.error('handleSocketError() [error:%o]', error);
