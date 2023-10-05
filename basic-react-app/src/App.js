666764a42e430ea76a6335b8b5475d900727c0e2
KJY-kpu <rntlls1366@gmail.com>
2023-10-05 16:25:14 +0900

    💄	: 메인 페이지 디자인 적용

--- basic-react-app/src/App.js
+++ basic-react-app/src/App.js
@@ -3,7 +3,6 @@ import TeacherScreen from './components/TeacherScreen';
 import {BrowserRouter,Route,Routes} from 'react-router-dom';
 import Teacher from './components/Teacher';
 import Student from './components/Student';
-import Main from './components/Main';
 import Home from './components/Home';
 
 function App() {
@@ -12,8 +11,7 @@ function App() {
     <BrowserRouter> 
        <Routes>  
 
-        <Route path="/" element={<Main/>} >  </Route>
-        <Route path="/home" element={<Home/>}> </Route>
+        <Route path="/" element={<Home/>} >  </Route>
         <Route path="/teacher/:code/:nickname" element={<Teacher/>}>  </Route> 
         <Route path="/student/:code/:nickname" element={<Student/>}>  </Route>
 
