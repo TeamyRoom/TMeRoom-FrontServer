20676b4d8e26f4fca7cb0e33467f60fbf3c35f71
KJY-kpu <rntlls1366@gmail.com>
2023-10-16 16:17:31 +0900

    🐛	: script 태그 위치 수정

--- basic-react-app/src/App.js
+++ basic-react-app/src/App.js
@@ -1,20 +1,19 @@
 import React, { useState, useEffect, useRef } from 'react';
 import TeacherScreen from './components/TeacherScreen';
-import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import LectureFrame from './components/LectureFrame';
 
 function App() {
 
   return (
-    <BrowserRouter> 
-       <Routes>  
-       <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-
-        <Route path="/" element={<Home/>} >  </Route>
-        <Route path="/lecture/:lecturecode" element={<LectureFrame/>}>  </Route> 
-       </Routes>
-    </BrowserRouter> 
+    <BrowserRouter>
+      <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+      <Routes>
+        <Route path="/" element={<Home />} >  </Route>
+        <Route path="/lecture/:lecturecode" element={<LectureFrame />}>  </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
