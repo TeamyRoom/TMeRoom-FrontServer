84f73c97b3a48ac984645353fd61ad86b33fec39
KJY-kpu <rntlls1366@gmail.com>
2023-10-20 14:41:56 +0900

    ⚡️	: 수강중인 강의 컴포넌트 생성

---
+++ src/components/MyLecture.js
@@ -0,0 +1,120 @@
+import "../css/MyLecture.css";
+
+export default function MyLecture() {
+
+    return (
+        <div class="table-box">
+            <h1 class="Title">전체 강의 목록</h1>
+            <div class="table-row table-head">
+                <div class="table-cell first-cell">
+                    <p>강좌명</p>
+                </div>
+                <div class="table-cell">
+                    <p>강사</p>
+                </div>
+                <div class="table-cell last-cell">
+                    <p>강좌신청</p>
+                </div>
+            </div>
+
+
+            <div class="table-row">
+                <div class="table-cell first-cell">
+                    <p>카카오 클라우드 스쿨 3기 개발자반</p>
+                </div>
+                <div class="table-cell">
+                    <p>김주효</p>
+                </div>
+                <div class="table-cell last-cell">
+                    <button class="apply">신청하기</button>
+                </div>
+            </div>
+
+
+            <div class="table-row">
+                <div class="table-cell first-cell">
+                    <p>카카오 클라우드 스쿨 3기 엔지니어반</p>
+                </div>
+                <div class="table-cell">
+                    <p>김아무개</p>
+                </div>
+                <div class="table-cell last-cell">
+                    <button class="apply">신청하기</button>
+                </div>
+            </div>
+
+            <div class="table-row">
+                <div class="table-cell first-cell">
+                    <p>AWS CLOUD SCHOOL</p>
+                </div>
+                <div class="table-cell">
+                    <p>김강사</p>
+                </div>
+                <div class="table-cell last-cell">
+                    <button class="apply">신청하기</button>
+                </div>
+            </div>
+
+            <div class="table-row">
+                <div class="table-cell first-cell">
+                    <p>AWS CLOUD SCHOOL 2기</p>
+                </div>
+                <div class="table-cell">
+                    <p>제프 베이조스</p>
+                </div>
+                <div class="table-cell last-cell">
+                    <button class="apply">신청하기</button>
+                </div>
+            </div>
+
+            <div class="table-row">
+                <div class="table-cell first-cell">
+                    <p>NETFLIX 3D/FX 강의</p>
+                </div>
+                <div class="table-cell">
+                    <p>999</p>
+                </div>
+                <div class="table-cell last-cell">
+                    <button class="apply">신청하기</button>
+                </div>
+            </div>
+
+
+            <div class="table-row">
+                <div class="table-cell first-cell">
+                    <p>개설예정입니다.</p>
+                </div>
+                <div class="table-cell">
+                    <p>구인중입니다.</p>
+                </div>
+                <div class="table-cell last-cell">
+                    <button class="apply">알림 신청하기</button>
+                </div>
+            </div>
+
+            <div class="table-row">
+                <div class="table-cell first-cell">
+                    <p>개설예정입니다.</p>
+                </div>
+                <div class="table-cell">
+                    <p>구인중입니다.</p>
+                </div>
+                <div class="table-cell last-cell">
+                    <button class="apply">신청하기</button>
+                </div>
+            </div>
+
+            <div class="table-row">
+                <div class="table-cell first-cell">
+                    <p>개설예정입니다.</p>
+                </div>
+                <div class="table-cell">
+                    <p>구인중입니다.</p>
+                </div>
+                <div class="table-cell last-cell">
+                    <button class="apply">신청하기</button>
+                </div>
+            </div>
+        </div>
+    )
+}
